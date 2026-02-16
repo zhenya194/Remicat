08831fc6416229e549d42f5ef0fb4d6cd235d8e5
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-16 18:15:37 +0200

    Update buttons

---
+++ scripts/indexbuttons.js
@@ -0,0 +1,9 @@
+function showAbout() {
+    document.getElementById("about").style.display = "block"
+    document.getElementById("config").style.display = "none"
+}
+
+function showConfig() {
+    document.getElementById("about").style.display = "none"
+    document.getElementById("config").style.display = "block"
+}
