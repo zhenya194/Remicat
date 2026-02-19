9c1905f363b39e173142c0102d5ba6351f965bb2
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-19 07:24:47 +0200

    Update paths

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 770,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true 
+    }
+  });
+
+  win.loadFile('pages/index.html');
+}
+
+app.whenReady().then(createWindow);
+
 function showAbout() {
     document.getElementById("about").style.display = "block"
     document.getElementById("config").style.display = "none"
